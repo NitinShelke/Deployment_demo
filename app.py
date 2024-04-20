81149a0c286eeaba87dc92a7360bfe7167867181
Nitin <60922929+NitinShelke@users.noreply.github.com>
2024-04-20 16:17:06 +0530

    message chnged

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return('Hare Krishna Hare Krishna<br>Krishna Krishna Hare Hare<br>Hare Rama Hare Rama<br>Rama Rama Hare Hare')
+    return('Shril Prabhuopad ki jay!!!')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
